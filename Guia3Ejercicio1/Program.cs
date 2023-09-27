cc5e906aa1aedc450581722b5ba12d6c382954d2
sujey22 <da0089032022@unab.edu.sv>
2023-09-26 20:06:05 -0600

    Create Program.cs

---
+++ Guia3Ejercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class EjemplodePi1
+{
+    public static void Main()
+    {
+        double valordePi = Math.PI;
+        Console.WriteLine("El valor de pi en origial es " + valordePi);
+        Console.WriteLine();
+        double truncate = Math.Truncate(valordePi);
+        Console.WriteLine("El valor truncado es de: " + truncate);
+        Console.WriteLine();
+        double piRedondhacArriba = Math.Ceiling(valordePi);
+        Console.WriteLine("El valor redondeado hacia arriba es: " + piRedondhacArriba);
+        Console.WriteLine();
+        double piRedondhacAbajo = Math.Floor(valordePi);
+        Console.WriteLine("El valor redondeado hacia abajo es: " + piRedondhacAbajo);
+        Console.WriteLine();
+        Console.WriteLine("Hecho por Merlin Sujey Duarte Argueta");
+
+    }
+}
