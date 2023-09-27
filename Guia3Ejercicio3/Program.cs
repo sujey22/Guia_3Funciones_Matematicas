c7b75b882a84522044459096874e27a622511eaf
sujey22 <da0089032022@unab.edu.sv>
2023-09-26 22:46:25 -0600

    Create Program.cs

---
+++ Guia3Ejercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class SumadeCuadrados
+{
+    public static void Main()
+    {
+        int num = 20;
+        int suma = 0;
+
+        for (int i = 1;i <= num; i++)
+        {
+            int cuadrado = i*i;
+            suma += cuadrado;
+        }
+
+        Console.WriteLine("La suma acumulada de los cuadrados de los primeros 20 digitos es: " + suma);
+        Console.WriteLine();
+        Console.WriteLine("Hecho por Merlin Sujey Duarte Argueta");
+
+    }
+}
