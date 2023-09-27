e3d28c85bf2445a12c5efc544d8739db11b246d3
sujey22 <da0089032022@unab.edu.sv>
2023-09-26 22:07:11 -0600

    Create Program.cs

---
+++ Guia3Ejercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class EjemplodeEuler2
+{
+    public static void Main()
+    {
+        double valordeE = Math.E;
+        Console.WriteLine("El valor de euler en origial es " + valordeE);
+        Console.WriteLine();
+        double eulerRedondeado = Math.Round(valordeE, 10);
+        Console.WriteLine("El valor de euler redondeado es: " + eulerRedondeado);
+        Console.WriteLine();
+        Console.WriteLine("Hecho por Merlin Sujey Duarte Argueta");
+
+    }
+}
